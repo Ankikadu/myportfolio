c0ce49bc0d1cb6106dc5b0fbb0abaafb68b13dd2
Ankikadu <97692472+Ankikadu@users.noreply.github.com>
2022-05-31 16:37:13 -0700

    added

---
+++ src/Components/ProjectsSection.jsx
@@ -0,0 +1,56 @@
+import { ProjectCard } from "./ProjectCard"
+import styles from "./Styles/ProjectsSection.module.css"
+import { SiReact } from "react-icons/si"
+import { SiJavascript } from "react-icons/si"
+import { SiHtml5 } from "react-icons/si"
+import { SiCss3 } from "react-icons/si"
+import { SiRedux } from "react-icons/si"
+import Fade from "react-reveal/Fade"
+const projects = [
+  {
+    name: "Zappos.com clone",
+    img: "/zappos.png",
+    // link: " ",
+    git: "https://github.com/Ankikadu/Zappos.git",
+    about:
+      "Zappos is a website use for online shopping specially for women clothes. Implemented features to get data from backend.",
+    stacks: [
+      <SiReact className={styles.stackIcon} />,
+      <SiRedux className={styles.stackIcon} />,
+       <SiJavascript className={styles.stackIcon} />,
+      <SiCss3 className={styles.stackIcon} />,
+ 
+    ],
+  },
+  {
+    name: "BestBuy.com clone",
+    img: "/Bestbuy.png",
+    // link: " ",
+    git: "https://github.com/Ankikadu/Best_buy_wesite_clone_project.git",
+    about:
+      " BestBuy is a online shooping website. This was a team project where I with my teammates had created a shopping platform specially for electronics product. Tech Stack which we used in this project is HTML, CSS, JS.",
+    stacks: [
+      <SiJavascript className={styles.stackIcon} />,
+      <SiHtml5 className={styles.stackIcon} />,
+      <SiCss3 className={styles.stackIcon} />,
+       
+    ],
+  }
+  
+]
+export function ProjectsSection() {
+  return (
+    <div className={styles.projectsSectionCont} id="projects">
+      <Fade bottom>
+        <p>Projects</p>
+      </Fade>
+      <div className={styles.projectGrid}>
+        {projects.map((project) => (
+          <Fade bottom>
+            <ProjectCard {...project} />
+          </Fade>
+        ))}
+      </div>
+    </div>
+  )
+}
